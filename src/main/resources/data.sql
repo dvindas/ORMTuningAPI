bedd4735daf377c68427b6613462e56d8e18e654
dvindas <41488048+dvindas@users.noreply.github.com>
2024-10-12 13:45:05 -0600

    Fixing test data.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,14 +10,10 @@
 INSERT INTO invoice_details (id, product_name, quantity, price, total, invoice_id) VALUES
                                                                                        (1, 'Product A', 2, 100.00, 200.00, 1),
                                                                                        (2, 'Product B', 2, 40.00, 80.00, 1),
-
                                                                                        (3, 'Product C', 1, 300.00, 300.00, 2),
-                                                                                       (4, 'Product D', 2, 95.00, 190.00, 2),   -
-
+                                                                                       (4, 'Product D', 2, 95.00, 190.00, 2),
                                                                                        (5, 'Product E', 2, 250.00, 500.00, 3),
                                                                                        (6, 'Product F', 1, 20.00, 20.00, 3),
-
                                                                                        (7, 'Product G', 3, 200.00, 600.00, 4),
-
                                                                                        (8, 'Product H', 4, 150.00, 600.00, 5),
-                                                                                       (9, 'Product I', 2, 20.00, 40.00, 5);    
+                                                                                       (9, 'Product I', 2, 20.00, 40.00, 5);
